eec3c251ab4200a75d481c72bf44fc3e8fd23031
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-08-28 11:09:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const axios = require("axios")
-
-  let API_KEY = "mi5qSSqdhmrNXBjLq5MBMwuqcS0q8aE4u52fwqrG8CkrBjjksgdV8ZblHdh4ThtDqQVFapfOwrCqadcTH4sJIMhQgEcWpc0bK_9ms_rJ1H-xMT1Amp4tmH_PhAg3X3Yx"
-
-  // REST
-  let yelpREST = axios.create({
-    baseURL: "https://api.yelp.com/v3/",
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      "Content-type": "application/json",
-    },
-  })
-
-  yelpREST('businesses/search/', { params: { location: 'San Francisco', categories: 'Parking' } }).then(({ data }) => {
-    console.log('data:', data)
-  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
